b58facf9412b5dc23e5efc7e4d561ca15ca75f6c
theoleonet <theo.leonet@student.hepl.be>
2022-03-15 20:40:40 +0100

    :bento: Updated main.ts
    Cleaned the code, added the right command, added output extension input field.

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,14 +1,11 @@
 //Variables declarations
-const forms: HTMLUListElement = document.querySelector('.forms') as HTMLUListElement;
+const formsContainer: HTMLUListElement = document.querySelector('.forms') as HTMLUListElement;
 const newCardButton: HTMLButtonElement = document.querySelector('.new-card') as HTMLButtonElement;
-const createCommandButton : HTMLButtonElement = document.querySelector('.create-command') as HTMLButtonElement;
+const createCommandButton: HTMLButtonElement = document.querySelector('.create-command') as HTMLButtonElement;
 let idNumber: number = 1;
-let sizesArray: string[][] = [] as string[][];
-let finalString: string;
 
 //functions calls
 createCard();
-let formArray = Array.from(document.querySelectorAll('.forms__form'));
 
 //EventListeners
 newCardButton.addEventListener('click', (e)=>{
@@ -19,47 +16,41 @@ newCardButton.addEventListener('click', (e)=>{
 
 createCommandButton.addEventListener('click', (e)=>{
     e.preventDefault();
-    finalString = '';
-    let formArray = Array.from(document.querySelectorAll('.forms__form'));
-    for (const t of formArray) {
-            let input: HTMLInputElement = t.querySelector('.input') as HTMLInputElement;
-            let output: HTMLInputElement = t.querySelector('.output') as HTMLInputElement;
-            let sizes: HTMLInputElement = t.querySelector('.sizes') as HTMLInputElement;
-            let sizesArray: string[] = sizes.value.split(',') as string[];
-        if (formArray.indexOf(t) < formArray.length-1) {
-            for (const size of sizesArray) {
-                finalString += `mogrify ${input.value} ${size} ${output.value} && `
-            }
-        }
-        else {
-            for (const size of sizesArray) {
-                if (sizesArray.indexOf(size) < sizesArray.length-1){
-                    finalString += `mogrify ${input.value} ${size} ${output.value} && `
-                }
-                else{
-                    finalString += `mogrify ${input.value} ${size} ${output.value}`
-                }
-            }
-        }
+    let outputCommand: string = '';
+    let forms = Array.from(document.querySelectorAll('.forms__form'));
+    for (const form of forms) {
+        let inputInput: HTMLInputElement = form.querySelector('.input') as HTMLInputElement;
+        let outputInput: HTMLInputElement = form.querySelector('.output') as HTMLInputElement;
+        let sizesInput: HTMLInputElement = form.querySelector('.sizes') as HTMLInputElement;
+        let extensionInput: HTMLInputElement = form.querySelector('.extension') as HTMLInputElement;
+        let sizes: string[] = sizesInput.value.split(',') as string[];
+        if (forms.indexOf(form) < forms.length-1) for (const size of sizes) outputCommand += `convert '${inputInput.value}' -set filename:fn '%[basename]-${size}' -resize ${size}^ -quality 100 '${outputInput.value}/%[filename:fn].${extensionInput.value}' && `
+        else for (const size of sizes) sizes.indexOf(size) < sizes.length-1 ? outputCommand += `convert '${inputInput.value}' -set filename:fn '%[basename]-${size}' -resize ${size}^ -quality 100 '${outputInput.value}/%[filename:fn].${extensionInput.value}' && ` : outputCommand += `convert '${inputInput.value}' -set filename:fn '%[basename]-${size}' -resize ${size}^ -quality 100 '${outputInput.value}/%[filename:fn].${extensionInput.value}'`;
     }
-    console.log(finalString)
+    navigator.clipboard.writeText(outputCommand)
+        .then(()=> alert('Text copied to clipboard'))
+        .catch((err: ErrorCallback)=> alert(`Error in copying text: ${err}`));
 })
 
 //Functions declarations
 function createCard(): void{
-    forms.insertAdjacentHTML('afterbegin', `
+    formsContainer.insertAdjacentHTML('afterbegin', `
         <li class="forms__form">
             <div class="input__container">
                 <label for="input-${idNumber}" class="form__label input__label">Input directory :</label>
-                <input type="text" class="form__input input" id="input-${idNumber}" placeholder="Ex: USERS/yourname/prohects/myImages">
+                <input type="text" class="form__input input" id="input-${idNumber}" placeholder="Ex: Users/yourname/projects/myImagesInput/*.jpg">
             </div>
             <div class="input__container">
                 <label for="output-${idNumber}" class="form__label output__label">Output directory :</label>
-                <input type="text" class="form__input output" id="output-${idNumber}" placeholder="Ex: USERS/yourname/prohects/myImages">
+                <input type="text" class="form__input output" id="output-${idNumber}" placeholder="Ex: Users/yourname/projects/myImagesOutput">
+            </div>
+            <div class="input__container">
+                <label for="extension-${idNumber}" class="form__label extension__label">Output file extension :</label>
+                <input type="text" class="form__input extension" id="extension-${idNumber}" placeholder="Ex: jpg, png, ...">
             </div>
             <div class="input__container">
                 <label for="sizes-${idNumber}" class="form__label sizes__label">Sizes :</label>
-                <input type="text" class="form__input sizes" id="sizes-${idNumber}" placeholder="Ex: 1920x1080, 1444x326, ...">
+                <input type="text" class="form__input sizes" id="sizes-${idNumber}" placeholder="Ex: 1920x1080, 1440x810, ...">
             </div>   
         </li>
     `)
