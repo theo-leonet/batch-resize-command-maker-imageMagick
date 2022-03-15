a4dfee9e5e5f6684dd8facfd770d0369d0c67d1d
theoleonet <theo.leonet@student.hepl.be>
2022-03-15 17:31:27 +0100

    :bento: Updated main.ts
    Added the command creation logic. Have to refactor it because code is disgusting :nauseated_face:.

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,10 +1,14 @@
 //Variables declarations
 const forms: HTMLUListElement = document.querySelector('.forms') as HTMLUListElement;
 const newCardButton: HTMLButtonElement = document.querySelector('.new-card') as HTMLButtonElement;
-let idNumber = 0;
+const createCommandButton : HTMLButtonElement = document.querySelector('.create-command') as HTMLButtonElement;
+let idNumber: number = 1;
+let sizesArray: string[][] = [] as string[][];
+let finalString: string;
 
 //functions calls
 createCard();
+let formArray = Array.from(document.querySelectorAll('.forms__form'));
 
 //EventListeners
 newCardButton.addEventListener('click', (e)=>{
@@ -13,18 +17,50 @@ newCardButton.addEventListener('click', (e)=>{
     createCard();
 })
 
+createCommandButton.addEventListener('click', (e)=>{
+    e.preventDefault();
+    finalString = '';
+    let formArray = Array.from(document.querySelectorAll('.forms__form'));
+    for (const t of formArray) {
+            let input: HTMLInputElement = t.querySelector('.input') as HTMLInputElement;
+            let output: HTMLInputElement = t.querySelector('.output') as HTMLInputElement;
+            let sizes: HTMLInputElement = t.querySelector('.sizes') as HTMLInputElement;
+            let sizesArray: string[] = sizes.value.split(',') as string[];
+        if (formArray.indexOf(t) < formArray.length-1) {
+            for (const size of sizesArray) {
+                finalString += `mogrify ${input.value} ${size} ${output.value} && `
+            }
+        }
+        else {
+            for (const size of sizesArray) {
+                if (sizesArray.indexOf(size) < sizesArray.length-1){
+                    finalString += `mogrify ${input.value} ${size} ${output.value} && `
+                }
+                else{
+                    finalString += `mogrify ${input.value} ${size} ${output.value}`
+                }
+            }
+        }
+    }
+    console.log(finalString)
+})
+
 //Functions declarations
-function createCard(){
-    forms.innerHTML += `
-        <li class="forms__item">
-            <form action="#" class="forms__form">
-                <label for="input-${idNumber}" class="form__label form__input-label">input</label>
-                <input type="text" id="input-${idNumber}" class="form__input">
-                <label for="output-${idNumber}" class="form__label form__output-label">output</label>
-                <input type="text" id="output-${idNumber}" class="form__output">
-                <label for="sizes-${idNumber}" class="form__label form__sizes-label">sizes</label>
-                <input type="text" id="sizes-${idNumber}" class="form__sizes">
-            </form>
-        <li/>
-    `
+function createCard(): void{
+    forms.insertAdjacentHTML('afterbegin', `
+        <li class="forms__form">
+            <div class="input__container">
+                <label for="input-${idNumber}" class="form__label input__label">Input directory :</label>
+                <input type="text" class="form__input input" id="input-${idNumber}" placeholder="Ex: USERS/yourname/prohects/myImages">
+            </div>
+            <div class="input__container">
+                <label for="output-${idNumber}" class="form__label output__label">Output directory :</label>
+                <input type="text" class="form__input output" id="output-${idNumber}" placeholder="Ex: USERS/yourname/prohects/myImages">
+            </div>
+            <div class="input__container">
+                <label for="sizes-${idNumber}" class="form__label sizes__label">Sizes :</label>
+                <input type="text" class="form__input sizes" id="sizes-${idNumber}" placeholder="Ex: 1920x1080, 1444x326, ...">
+            </div>   
+        </li>
+    `)
 }
